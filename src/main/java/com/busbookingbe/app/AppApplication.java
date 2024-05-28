2971c32faf3a3118f8c7849743a5320e309293c1
VengateshPrasad <venkuvenky2@gmail.com>
2024-05-28 15:20:49 +0530

    added swagger

--- src/main/java/com/busbookingbe/app/AppApplication.java
+++ src/main/java/com/busbookingbe/app/AppApplication.java
@@ -1,17 +1,25 @@
 package com.busbookingbe.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url= "https:bookingManagement1.com"),
+		@Server(url= "https:bookingManagement2.com") },
+		info = @Info(title = "Booking Methods", version = "v1",
+		description = "CRUD Operations in Bus Booking"))
 public class AppApplication {
 
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
