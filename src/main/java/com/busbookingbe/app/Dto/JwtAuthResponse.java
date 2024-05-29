9e142c63d6e4543678634ab3f1cbb7b1a2e8d23f
VengateshPrasad <venkuvenky2@gmail.com>
2024-05-29 15:19:30 +0530

    added security

---
+++ src/main/java/com/busbookingbe/app/Dto/JwtAuthResponse.java
@@ -0,0 +1,16 @@
+package com.busbookingbe.app.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtAuthResponse {
+    private String accessToken;
+    private String role;
+    private String userName;
+    private Long userId;
+    private String tokenType = "Bearer";
+}
