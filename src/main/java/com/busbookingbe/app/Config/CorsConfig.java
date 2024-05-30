bfe592cd2b89a7d7ba5242efa7386110ff2d3e43
VengateshPrasad <venkuvenky2@gmail.com>
2024-05-30 16:24:36 +0530

    added corsconfig

---
+++ src/main/java/com/busbookingbe/app/Config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.busbookingbe.app.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow all origins (for production, specify allowed origins)
+        config.addAllowedOrigin("*");
+
+        // Allow all headers
+        config.addAllowedHeader("*");
+
+        // Allow HTTP methods you need (GET, POST, etc.)
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/com/busbookingbe/app/Config/SecurityConfig.java
+++ src/main/java/com/busbookingbe/app/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.busbookingbe.app.Security.JwtTokenFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,24 +13,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @AllArgsConstructor
 public class SecurityConfig {
 
     JwtTokenFilter jwtTokenFilter;
 
+    private final CorsFilter corsFilter;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf-> csrf.disable())
-                .cors(cors-> cors.disable())
+        http.addFilterBefore(corsFilter, SessionManagementFilter.class)
+                .csrf(csrf-> csrf.disable())
+//                .cors(cors-> cors.disable())
                 .anonymous(anonymous->anonymous.disable());
         http.authorizeHttpRequests(auth->auth
 //                .requestMatchers("/api/v1/passenger/**").authenticated()
-                .requestMatchers("/api/v1/booking/**", "/api/auth/**", "/api/v1/bus/**").permitAll()
+                .requestMatchers( "/api/auth/**", "/api/v1/bus/**").permitAll()
                 .requestMatchers("swagger-ui/**", "/resources/**", "/static/**", "v3/api-docs/**").permitAll() //Swagger API
                 .anyRequest().authenticated());
         http.sessionManagement(session ->
