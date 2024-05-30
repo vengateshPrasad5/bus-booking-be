65a1fd0bebdcee3f3bd73f3a029bcc996cdac862
VengateshPrasad <venkuvenky2@gmail.com>
2024-05-30 16:30:54 +0530

    findBySource... method modified

--- src/main/java/com/busbookingbe/app/Respository/BusRepository.java
+++ src/main/java/com/busbookingbe/app/Respository/BusRepository.java
@@ -1,6 +1,5 @@
 package com.busbookingbe.app.Respository;
 
-import com.busbookingbe.app.Dto.BusDTO;
 import com.busbookingbe.app.Entity.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,20 +7,18 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 @Repository
 public interface BusRepository extends JpaRepository<Bus,Long> {
 
-//    @Query(value = "SELECT * FROM bus WHERE source = :source AND " +
-//            "destination = :destination AND DATE(departure_time) = :departureDate",
-//            nativeQuery = true)
-    List<Bus> findBySourceAndDestinationAndDepartureTime(
-            String source,
-            String destination,
-            Date departureTime);
+    @Query(value = "SELECT * FROM bus WHERE source =:source AND " +
+            "destination =:destination AND departure_date =:departureDate",
+            nativeQuery = true)
+    List<Bus> findBySourceAndDestinationAndDepartureDate(
+            @Param("source") String source ,
+            @Param("destination") String destination,
+            @Param("departureDate") LocalDate departureTime);
 
     @Query("SELECT DISTINCT source FROM Bus")
     List<String> findDistinctSource();
