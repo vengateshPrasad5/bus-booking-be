385c892cccd61dd8361ca8d7182a69a1ac30947b
Vengatesh <venkuvenky2@gmail.com>
2024-06-26 08:47:26 +0530

    exception handled

--- src/main/java/com/busbookingbe/app/Controller/BookingController.java
+++ src/main/java/com/busbookingbe/app/Controller/BookingController.java
@@ -1,7 +1,9 @@
 package com.busbookingbe.app.Controller;
 
 import com.busbookingbe.app.Dto.BookingDTO;
+import com.busbookingbe.app.Exception.ResourceNotFoundException;
 import com.busbookingbe.app.Service.ServiceImpl.BookingServiceImpl;
+import com.razorpay.RazorpayException;
 import jakarta.annotation.security.PermitAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -13,23 +15,31 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
-@CrossOrigin("*")
+@CrossOrigin(origins = {"*","http://127.0.0.1:5173/"})
 @RestController
 @RequestMapping("/api/v1/booking")
 public class BookingController {
     @Autowired
     BookingServiceImpl bookingServiceImpl;
-
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
     @GetMapping("/getBookingListByDate")
     public ResponseEntity<List<BookingDTO>>findByBookingList(@RequestParam String reservationDate) {
-        List<BookingDTO> bookingList = bookingServiceImpl.getBookingByReservationDate(reservationDate);
-        return new ResponseEntity<>(bookingList, HttpStatus.OK);
+        try {
+            List<BookingDTO> bookingList = bookingServiceImpl.getBookingByReservationDate(reservationDate);
+            return new ResponseEntity<>(bookingList, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("BookingList Not Found");
+        }
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
     @PostMapping("/createBooking")
     public ResponseEntity<BookingDTO> createBooking(@RequestBody BookingDTO bookingDTO) {
-        BookingDTO booking = bookingServiceImpl.createBooking(bookingDTO);
-        return new ResponseEntity<>(booking, HttpStatus.CREATED);
+        try {
+            BookingDTO booking = bookingServiceImpl.createBooking(bookingDTO);
+            return new ResponseEntity<>(booking, HttpStatus.CREATED);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Booking Unsuccessful!!!");
+        }
     }
 }
--- src/main/java/com/busbookingbe/app/Controller/PassengerController.java
+++ src/main/java/com/busbookingbe/app/Controller/PassengerController.java
@@ -2,6 +2,7 @@ package com.busbookingbe.app.Controller;
 
 import com.busbookingbe.app.Dto.PassengerDTO;
 import com.busbookingbe.app.Entity.Passenger;
+import com.busbookingbe.app.Exception.ResourceNotFoundException;
 import com.busbookingbe.app.Service.ServiceImpl.PassengerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,13 +19,21 @@ public class PassengerController {
 
     @GetMapping("/getPassengerList")
     public ResponseEntity<List<PassengerDTO>> passengerList(@RequestParam Long customerId){
-        List<PassengerDTO>passengerList = passengerServiceImpl.getPassengerCustomerId(customerId);
-        return new ResponseEntity<>(passengerList, HttpStatus.OK);
+        try {
+            List<PassengerDTO>passengerList = passengerServiceImpl.getPassengerCustomerId(customerId);
+            return new ResponseEntity<>(passengerList, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException(e.getMessage());
+        }
     }
 
     @GetMapping("/getBookingListByCustomer")
     public ResponseEntity<List<Passenger>>bookingListByCustomer(@RequestParam Long userId) {
-        List<Passenger> bookingList = passengerServiceImpl.getBookingByCustomerId(userId);
-        return new ResponseEntity<>(bookingList, HttpStatus.OK);
+        try {
+            List<Passenger> bookingList = passengerServiceImpl.getBookingByCustomerId(userId);
+            return new ResponseEntity<>(bookingList, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException(e.getMessage());
+        }
     }
 }
